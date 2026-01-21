52e67620df30ed5e45ce1504dd15504ee569f2f1
Gabriel González Ramírez <gabrosblack8@gmail.com>
2026-01-21 16:46:17 -0600

    Correcion para imagen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 // Importamos la imagen desde la carpeta assets
-import miImagen from './assets/img/logo-cid-estn.jpg' 
 
 function App() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f0f0f0' }}>
       {/* Aquí mostramos la imagen */}
       <img 
-        src={miImagen} 
+        src="/PruebaCidetec/logo-cid-estn.jpg" 
         alt="Una imagen genial" 
         style={{ maxWidth: '90%', height: 'auto', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0,0,0,0.2)' }} 
       />
